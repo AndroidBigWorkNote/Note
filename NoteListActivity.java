81de977031b10766326f63188f826edb38a68651
Janson-Yan <abcde1220887663@qq.com>
2018-12-13 19:30:19 +0800

    create project

---
+++ NoteListActivity.java
@@ -0,0 +1,13 @@
+package com.example.apple.notework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+    }
+}
